9eb9ff9d7e35fdfaa064c50cb6517ef955c0167b
alexisblopez <alopez78@uic.edu>
2019-12-08 17:12:39 -0600

    added sklearn

--- support_vector_machine_sklearn.py
+++ support_vector_machine_sklearn.py
@@ -1,23 +1,35 @@
 import numpy as np
-from sklearn import preprocessing, cross_validation, neighbors, svm
+from sklearn import svm
+from sklearn.model_selection import train_test_split
 import pandas as pd
 
-df = pd.read_csv('breast-cancer-wisconsin.data.txt')
-df.replace('?',-99999, inplace=True)
-df.drop(['id'], 1, inplace=True)
 
-X = np.array(df.drop(['class'], 1))
-y = np.array(df['class'])
+def supportVectorMachine():
+    df = pd.read_csv('challenge_response_pairs.csv')
+    print(df)
 
-X_train, X_test, y_train, y_test = cross_validation.train_test_split(X, y, test_size=0.2)
+    print(df[1:3])
 
-clf = svm.SVC()
+    X = np.array(df.iloc[:, :-1])
 
-clf.fit(X_train, y_train)
-confidence = clf.score(X_test, y_test)
-print(confidence)
+    print(X)
 
-example_measures = np.array([[4,2,1,1,1,2,3,2,1]])
-example_measures = example_measures.reshape(len(example_measures), -1)
-prediction = clf.predict(example_measures)
-print(prediction)
+    y = np.array(df.iloc[:, -1])
+    print(y)
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+    clf = svm.SVC()
+
+    clf.fit(X_train, y_train)
+    confidence = clf.score(X_test, y_test)
+    print(confidence)
+
+    example_measures = np.array([[4, 2, 1, 1, 1, 2, 3, 2, 1]])
+    example_measures = example_measures.reshape(len(example_measures), -1)
+    prediction = clf.predict(example_measures)
+    print(prediction)
+
+
+if __name__ == "__main__":
+    supportVectorMachine()
